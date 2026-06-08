262dc0caab973b464008927db7e904d52d902147
jess-low <24135jl@hvhs.school.nz>
2026-06-08 22:36:30 +0000

    Jun 8, 2026, 10:36 PM

--- script.js
+++ script.js
@@ -9,19 +9,28 @@ var item = {
   blueberryMuffin: 0,
 }
 
+var price = {
+  flatWhite: 0,
+  orangeJuice: 0,
+  blueberryMuffin: 0,
+}
+
 function getFlatWhite(){
     const FLAT_WHITE = document.getElementById("flat");
     item.flatWhite = FLAT_WHITE.value;
+    price.flatWhite = (6 * item.flatWhite);
   }
 
 function getOrangeJuice(){
     const ORANGE_JUICE = document.getElementById("orange");
     item.orangeJuice = ORANGE_JUICE.value;
+    price.orangeJuice = (6 * item.orangeJuice);
   }
 
 function getBlueberryMuffin(){
     const BLUEBERRY_MUFFIN = document.getElementById("blueberry");
     item.blueberryMuffin = BLUEBERRY_MUFFIN.value;
+    price.blueberryMuffin = (6 * item.blueberryMuffin);
   }
 
 function calcFunc() {
@@ -33,11 +42,6 @@ function getName(){
   name = GET_NAME.value;
 }
 
-var price = {
-  flatWhite: (6 * item.flatWhite),
-  orangeJuice: (6 * item.orangeJuice),
-  blueberryMuffin: (5 * item.blueberryMuffin),
-}
   function getReceipt(){
     OUTPUT.innerHTML = "<h2> Name: " + name + "</h2>"
     OUTPUT.innerHTML += "<h3>These are the items on your receipt:</h3>"

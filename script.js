251b4c27249d41e4922ae2fbcbe26e4df27eea03
jess-low <24135jl@hvhs.school.nz>
2026-06-24 01:08:34 +0000

    Jun 24, 2026, 1:08 AM

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ function viewItems(){
     CLEAR_BUTTON.style.display = 'block';
     NAME_MONEY.style.display = 'block';
   } else {
-    OUTPUT.innerHTML = "<h3>I'm sorry, you don't have any items on your order. Try pressing 'Submit' when you enter an amount.</h3>";
+    OUTPUT.innerHTML = "<h3 style="font-family: Zen Old Micho;">I'm sorry, you don't have any items on your order. Try pressing 'Submit' when you enter an amount.</h3>";
   }
 }
 

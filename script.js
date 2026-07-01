e25ca55f5f5572eeece7cd0ebe56725b91c1f79b
jess-low <24135jl@hvhs.school.nz>
2026-07-01 01:10:23 +0000

    Jul 1, 2026, 1:10 AM

--- script.js
+++ script.js
@@ -157,15 +157,16 @@ function getQuicheLorraine(){
 //Check if name is valid
 function getName(){
     name = GET_NAME.value;
-    CLEAR_BUTTON.style.display = 'block';
+    OUTPUT.innerHTML = "<h3 style=\"font-family: 'Zen Old Mincho', serif;\">These are the items on your order:</h3>";
+    fastDisplay()
+    OUTPUT.innerHTML += "<h3 style=\"font-family: 'Zen Old Mincho', serif;\">Total: $" + item.finalNumber + "</h3>";
     RECEIPT_BUTTON.style.display = 'block';
+    CLEAR_BUTTON.style.display = 'block';
 }
 
 //Check if amount is valid
 function getMoney(){
   money = Number(GET_MONEY.value).toFixed(2);
-    CLEAR_BUTTON.style.display = 'block';
-    RECEIPT_BUTTON.style.display = 'block';
     OUTPUT.innerHTML = "<h3 style=\"font-family: 'Zen Old Mincho', serif;\">These are the items on your order:</h3>";
     fastDisplay()
     OUTPUT.innerHTML += "<h3 style=\"font-family: 'Zen Old Mincho', serif;\">Total: $" + item.finalNumber + "</h3>";

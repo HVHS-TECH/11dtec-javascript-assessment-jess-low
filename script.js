7e33aec169822cf6a8b4c3d1718a1383239480e0
jess-low <24135jl@hvhs.school.nz>
2026-07-23 09:55:43 +0000

    Jul 23, 2026, 9:55 AM

--- script.js
+++ script.js
@@ -353,6 +353,10 @@ function cancelAll(){
     OUTPUT.innerHTML = "<h3 style=\"font-family: 'Zen Old Mincho', serif;\">Please enter a name into the 'Name' section to complete your order.</h3>";
     CLEAR_BUTTON.style.display = 'none';
     RECEIPT_BUTTON.style.display = 'none';
+  } else if (GET_NAME.value === undefined){
+    OUTPUT.innerHTML = "<h3 style=\"font-family: 'Zen Old Mincho', serif;\">Please enter a name into the 'Name' section to complete your order.</h3>";
+    CLEAR_BUTTON.style.display = 'none';
+    RECEIPT_BUTTON.style.display = 'none';
   } else if (GET_MONEY.checkValidity() === false){
     OUTPUT.innerHTML = "<h3 style=\"font-family: 'Zen Old Mincho', serif;\">Please enter an amount into the 'Money' section to complete your order.</h3>";
     CLEAR_BUTTON.style.display = 'none';
